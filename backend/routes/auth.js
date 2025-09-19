c020c665e6328f6efe1bfe479f6074f82e49a4c6
Baladithya <balubaladithya162@gmail.com>
2025-09-19 14:55:44 +0530

    after er

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 
 const router = require('express').Router();
-const User = require('../models/user.models');
+const User = require('../models/user.model');
 
 router.route('/signup').post(async (req, res) => {
     const { username, password } = req.body;
