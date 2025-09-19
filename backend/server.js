f33cef8a3b3fa37d80cbfc4f31203622d37f28ae
Baladithya <balubaladithya162@gmail.com>
2025-09-19 14:43:44 +0530

    after errorsssssss

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,13 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
-
+const port = 3000;
+app.use(cors());
 app.use(express.json());
 const authRouter = require('./routes/auth');
 app.use('/auth', authRouter)
 
-const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/ecommerce';
+const uri ='mongodb://localhost:27017/mydatabase';
 mongoose.connect(uri)
 .then(() => console.log('MongoDB connected...'))
 .catch(err => console.error(err));
